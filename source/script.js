be561d0d3162bcc372ee985d05845428de26009b
Ahmed7121 <aqqanm@gmail.com>
2024-03-31 03:02:37 +0200

    all done

--- source/script.js
+++ source/script.js
@@ -1,14 +1,34 @@
 let navBar = document.querySelector("nav");
-let hamMenu = document.querySelector("img.ham")
-let navUl = document.querySelector("nav ul")
-let navUlA = document.querySelectorAll("nav ul a")
-let navUlALi = document.querySelectorAll("nav ul a li")
+let hamMenu = document.querySelector(".ham");
+let navUl = document.querySelector("nav ul");
+let navUlA = document.querySelectorAll("nav ul a");
+let navUlALi = document.querySelectorAll("nav ul a li");
+let contact = document.querySelector(".contact");
+let opened = document.querySelectorAll(".opened ul a");
 
+if (window.innerWidth < 800) {
+    navBar.style.transition = "none";
+}
 
-document.onload = onresize = function () {
+if (navBar.classList.contains("opened")) {
+    opened.forEach((e) => e.style.transition = "0.3s");
+}
+
+window.onresize = function () {
     if (this.innerWidth > 799) {
         navBar.style.transition = "none";
+        navUlA.forEach((e) => e.style.transition = "none");
+        navUlA.forEach((e) => {
+            e.removeAttribute("style")
+        });
+        navUlA.forEach((e) => e.style.transition = "none");
+        setTimeout(() => {
+            navUlA.forEach((e) => e.removeAttribute("style"));
+        }, 500);
+    } else {
+        navBar.style.transition = "none";
     }
+
 }
 hamMenu.addEventListener("click", function () {
     navBar.removeAttribute("style");
@@ -16,11 +36,10 @@ hamMenu.addEventListener("click", function () {
 })
 
 document.addEventListener("click", function (e) {
-    if (e.target !== navBar) {
-    }
     if (navBar.classList.contains("opened") && e.target !== navUl && e.target !== hamMenu && e.target !== navUlA &&
     e.target !== navUlALi[0] && e.target !== navUlALi[1]&& e.target !== navUlALi[2]&& e.target !== navUlALi[3] &&
     e.target !== navUlA[0] && e.target !== navUlA[1] && e.target !== navUlA[2] && e.target !== navUlA[3]) {
         navBar.classList.toggle("opened");
     }
-})
+});
+
